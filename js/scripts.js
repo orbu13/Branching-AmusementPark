b8288b56c8c0b82819045368b5f8a7676edb6c78
Orel Butbul <orelbutbul13@gmail.com>
2023-11-08 22:06:00 -0800

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // USER INTERFACE LOGIC
 
 function hideResults (){
+    document.getElementById("error-message").setAttribute("class", "hidden");
     document.getElementById("swings").setAttribute("class", "hidden");
     document.getElementById("coaster").setAttribute("class", "hidden");
     document.getElementById("tower").setAttribute("class", "hidden");
@@ -13,9 +14,20 @@ document.querySelector("form").onsubmit = function (e){
     const age = parseInt(document.querySelector("input#age").value);
     const height = parseInt(document.querySelector("input#height").value);
 
-    if (age>= 12 && height >= 60){
+    if (!age && !height){
+        document.getElementById("error-message").removeAttribute("class");
+
+        if (age>= 12 && height >= 60){
         document.getElementById("swings").removeAttribute("class");
         document.getElementById("coaster").removeAttribute("class");
         document.getElementById("tower").removeAttribute("class");
-    }
+        } else if (age >= 12 || height >= 60){
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+        } else if (age >= 6){
+        document.getElementById("swings").removeAttribute("class");
+        } else {
+        document.getElementById("sorry").removeAttribute("class");
+        }
+    } 
 };
